744d4a9e2b71f3be31be935a4ea0e85d3f1cabf4
blep111 <krexceloso@gmail.com>
2025-08-16 20:03:52 +0800

    Update Welcome.js

--- script/event/Welcome.js
+++ script/event/Welcome.js
@@ -24,7 +24,8 @@ module.exports.handleEvent = async function ({ api, event }) {
         const groupName = groupInfo.threadName || "this group";
         const background = groupInfo.imageSrc || "https://i.ibb.co/4YBNyvP/images-76.jpg";
 
-        const url = `https://hershey-api.onrender.com/api/welcome?username=${encodeURIComponent(name)}&avatarUrl=https://api-canvass.vercel.app/profile?uid=${senderID}&groupname=${encodeURIComponent(groupName)}&bg=${encodeURIComponent(background)}&memberCount=${memberCount}`;
+        const url = `https://ace-rest-api.onrender.com/api/welcome?username=Lance&avatarUrl=https://i.imgur.com/xwCoQ5H.jpeg&groupname=Ajironian&bg=https://i.ibb.co/4YBNyvP/images-76.jpg&memberCount=25/profile?uid=${senderID}&groupname=${encodeURIComponent(groupName)}&bg=${encodeURIComponent(background)}&memberCount=${memberCount}`;
+`;
 
         try {
             const { data } = await axios.get(url, { responseType: 'arraybuffer' });
