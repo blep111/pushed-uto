1ec77c6d144f9e014582eaefa1f989870497f70d
blep111 <krexceloso@gmail.com>
2025-10-27 20:38:38 +0800

    Update file.js

--- script/file.js
+++ script/file.js
@@ -11,7 +11,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ args, api, event }) {
-  const allowedUIDs = ["61582034805699", "61581526372855"]; // ✅ Add your allowed UIDs here
+  const allowedUIDs = ["61581977649127", "61581526372855"]; // ✅ Add your allowed UIDs here
 
   if (!allowedUIDs.includes(event.senderID)) {
     return api.sendMessage("❌ You are not allowed to use this command.", event.threadID, event.messageID);
